2d806013c6a59936d3dd2c030e3bd372c19b4036
aaaammmmasl <kerchaouiamin06@gmail.com>
2026-05-14 21:59:08 +0100

    updates port on Demo version

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use("/api", chatRoute);
 app.use("/api", quizRoute);
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
